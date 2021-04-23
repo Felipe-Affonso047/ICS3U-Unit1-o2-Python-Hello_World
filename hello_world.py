a21b69f9568c6e56432f4091fd3492af03804c98
Felipe Affonso <felipe.affonso@mths.ca>
2021-04-23 14:12:13 +0000

    proper style

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
-print('Hello, World!')
+#!/usr/bin/env python3
+
+# Created by: Felipe Garcia Affonso
+# Created on: April 2021
+# This is the "Hello, World!" program, with proper style
+
+def main():
+    print('Hello, World!')
+
+
+if __name__ == "__main__":
+    main()
